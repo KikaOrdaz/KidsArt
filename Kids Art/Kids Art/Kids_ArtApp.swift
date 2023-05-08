1e866a571f5893aafb35b53351acf83b0dc04f10
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2023-05-08 17:32:48 +0200

    File creation

---
+++ Kids Art/Kids Art/Kids_ArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kids_ArtApp.swift
+//  Kids Art
+//
+//  Created by Francisca Ordaz on 08/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Kids_ArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
