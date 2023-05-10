471e5fb60dd21250eae08afc063d7a789235e5b1
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2023-05-10 15:23:23 +0200

    Side Bar created - not functional

--- Kids Art/Kids Art/HomeView.swift
+++ Kids Art/Kids Art/HomeView.swift
@@ -15,67 +15,137 @@ struct HomeView: View {
         
         NavigationView(){
             
-            List{
-                Label("Dashboard", systemImage: "paintpalette.fill")
-            }
-            VStack{
-                HStack{
+            ZStack{
+                
+                Color(red: 211 / 255, green: 216 / 255, blue: 232 / 255)
+                    .ignoresSafeArea()
+                
+                
+                VStack{
+                    
+                    //THIS IS NOT A FUNCTIONING SEARCH BAR
+                    Capsule()
+                        .fill(Color(red: 250 / 255, green: 250 / 255, blue: 250 / 255))
+                        .frame(width: 262, height: 53)
+                        .overlay(
+                            HStack{
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(Color(red: 158 / 255, green: 158 / 255, blue: 158 / 255))
+                                
+                                Text("What are you looking for?")
+                                    .foregroundColor(Color(red: 158 / 255, green: 158 / 255, blue: 158 / 255))
+                            })
+                        .padding(30)
+                    
+                    
+                    
                     Circle()
-                        .fill(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                        .frame(width: 44, height: 44)
-                        .overlay(Text("🐵")).padding(40)
-                    VStack(alignment: .leading){
-                        Text("Nice to see you").foregroundColor(Color(red: 150 / 255, green: 150 / 255, blue: 150 / 255))
-                        Text("Josh")
-                            .fontWeight(.semibold)
+                        .fill(Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255))
+                        .frame(width: 98, height: 98)
+                        .overlay(Text("🐵")
+                            .font(.system(size: 54.02)))
+                    Text("Josh").font(.system(size : 39.89))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                    
+                    
+                    HStack(){
+                        Rectangle()
+                            .fill(Color(red: 0 / 255, green: 122 / 255, blue: 255 / 255))
+                            .frame(width: 123, height: 106.45)
+                            .cornerRadius(10)
+                            .overlay(
+                                VStack {
+                                    Image(systemName: "binoculars.fill")
+                                        .resizable()
+                                        .frame(width: 45.93, height: 35)
+                                        .foregroundColor(.white)
+                                        
+                                    Text("Explore")
+                                        .font(.system(size : 12.32))
+                                        .foregroundColor(.white)
+                                })
+                        
+                        Rectangle()
+                            .fill(Color(red: 266 / 255, green: 78 / 255, blue: 68 / 255))
+                            .frame(width: 123, height: 106.45)
+                            .cornerRadius(10)
+                            .overlay(
+                                VStack {
+                                    Text("🔥")
+                                        .font(.system(size : 53.37))
+                                    
+                                        
+                                    Text("Daily Challenge")
+                                        .font(.system(size : 12.32))
+                                        .foregroundColor(.white)
+                                })
                     }
+            
+                    
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: 198, height: 1)
+                    
+                    Rectangle()
+                        .fill(Color(red: 255 / 255, green: 149 / 255, blue: 0 / 255))
+                        .frame(width: 254.25, height: 104.15)
+                        .cornerRadius(10)
+                        .overlay(
+                            VStack {
+                                
+                                Text("Your Friends' Drawings")
+                                    .font(.system(size : 12.32))
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.white)
+                                    
+                                
+                                Text("🧑🏼‍🎨🧑🏾‍🎨👩🏽‍🎨👩🏻‍🎨")
+                                    .font(.system(size : 50))
+                                    .fontWeight(.semibold)
+                                    .foregroundColor(.white)
+                            })
+                    
+                    Rectangle()
+                        .fill(.white)
+                        .frame(width: 198, height: 1)
+                    
+                    Rectangle()
+                        .fill(Color(red: 255 / 255, green: 204 / 255, blue: 0 / 255))
+                        .frame(width: 254.25, height: 104.15)
+                        .cornerRadius(10)
+                    
                     
                     Spacer()
                     
+                    
+                        
+                    
                 }
                 
-                
-                //THIS IS NOT A FUNCTIONING SEARCH BAR
-                Capsule()
-                    .fill(Color(red: 250 / 255, green: 250 / 255, blue: 250 / 255))
-                    .frame(width: 348, height: 53)
-                    .overlay(
-                        HStack{
-                            Image(systemName: "magnifyingglass")
-                                .foregroundColor(Color(red: 158 / 255, green: 158 / 255, blue: 158 / 255))
-                                
-                        
-                            Text("What are you looking for?")
-                                .foregroundColor(Color(red: 158 / 255, green: 158 / 255, blue: 158 / 255))
-                                
-                        }
-                        
-                        
-                    )
-                
-                VStack(alignment: .leading){
-                    Text("Daily Challenge")
+            }
+            
+            VStack{
+                HStack{
+                    
+                    Text("Choose a Painting")
                         .fontWeight(.semibold)
-                        .font(.system(size: 20))
-                        
-                    Rectangle()
-                        .fill(Color(red: 236 / 255, green: 157 / 255, blue: 154 / 255))
-                        .frame(width: 375, height: 150)
-                        .cornerRadius(10)
-                        .overlay(Text("🔥")
-                            .font(.system(size : 100)))
+                        .padding(40)
+                        .font(.system(size: 29.56))
+                    
+                    Spacer()
                 }
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(40)
+                
+                Divider()
+                    .frame(width: 783)
                 
                 Spacer()
                 
-                
-                    
-                
             }
+            
+            
         
-        
+                
                     
                 
                 
