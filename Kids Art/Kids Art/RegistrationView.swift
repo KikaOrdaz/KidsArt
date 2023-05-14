c0bbf715eaf2e633b71f162dae073aea26202b4d
KikaOrdaz <45240183+KikaOrdaz@users.noreply.github.com>
2023-05-14 16:53:48 +0200

    Registration button disabled
    When there's no username, button doesn't work

--- Kids Art/Kids Art/RegistrationView.swift
+++ Kids Art/Kids Art/RegistrationView.swift
@@ -16,38 +16,6 @@ struct RegistrationView: View {
     
     
     var body: some View {
-<<<<<<< Updated upstream
-        VStack{
-            Circle()
-                .fill(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                .frame(width: 98, height: 98)
-                .overlay(Text("🐵")
-                    .font(.system(size: 54.02)))
-                .padding(.top)
-            Text("Enter your nickname")
-                .fontWeight(.regular)
-                .padding(.top, 25)
-            Divider()
-                .frame(width: 50, height: 2)
-                .overlay(Color.blue)
-            //Need to add a disclaimer to inform the user to not put any personal data
-            
-            
-            Button() {
-                        showingAlert = true
-            } label: {
-                TextField(text: $username, label: {
-                    Text("Nickname")
-                }) .padding()
-                .multilineTextAlignment(.center)
-                .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
-                .cornerRadius(50)
-                .frame(width: 400, height: 150)
-            }
-                .alert(isPresented: $showingAlert) {
-                    Alert(title: Text("Disclaimer"), message: Text("Don’t put any personal data in your nickname!"), dismissButton: .default(Text("Got it!")))
-                }
-=======
         
         NavigationStack{
             VStack{
@@ -57,9 +25,9 @@ struct RegistrationView: View {
                     .overlay(Text("🐵")
                         .font(.system(size: 54.02)))
                     .padding(.top)
-                Text("Enter your nickname")
-                    .fontWeight(.heavy)
-                    .padding(.top, 10)
+                Text("Enter your Nickname")
+                    .fontWeight(.regular)
+                    .padding(.top, 25)
                 Divider()
                     .frame(width: 50, height: 2)
                     .overlay(Color.blue)
@@ -72,6 +40,7 @@ struct RegistrationView: View {
                     TextField(text: $username, label: {
                         Text("Nickname")
                     })
+                    .padding()
                     .multilineTextAlignment(.center)
                     .background(Color(red: 217 / 255, green: 217 / 255, blue: 217 / 255))
                     .cornerRadius(50)
@@ -81,15 +50,15 @@ struct RegistrationView: View {
                     Alert(title: Text("Disclaimer"), message: Text("Don’t put any personal data in your nickname!"), dismissButton: .default(Text("Got it!")))
                 }
                 
+                /*NavigationLink {
+                    DashboardView()
+                } label: {
+                    
+                }*/
                 
                 
                 Button(){
-                    if(username == "") {
-                        emptyUsername = true
-                    } else {
-                        UserDefaults.standard.set(username, forKey: "username")
-                    }
-                    
+                    UserDefaults.standard.set(username, forKey: "username")
                     
                 } label: {
                     Capsule()
@@ -101,34 +70,20 @@ struct RegistrationView: View {
                                 .fontWeight(.bold)
                                 .foregroundColor(.white))
                 }
-                .alert(isPresented: $emptyUsername) {
-                    Alert(title: Text("Disclaimer"), message: Text("You can't start without a nickname!"), dismissButton: .default(Text("Okidoki")))
-                    
-                }
-                
-                NavigationLink{
-                     DashboardView()
-                 } label: {
-                     
-                     Capsule()
-                         .fill()
-                         .frame(width: 520, height: 93)
-                         .overlay(
-                             Text("Register")
-                                 .font(.system(size: 20))
-                                 .fontWeight(.bold)
-                                 .foregroundColor(.white))
-                 }
+                .disabled(username.isEmpty)
                 
                 
             }
+            
         }
     }
     
-    struct RegistrationView_Previews: PreviewProvider {
-        static var previews: some View {
-            RegistrationView()
->>>>>>> Stashed changes
-        }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
     }
 }
+
+
